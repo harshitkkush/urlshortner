583fa3a7cc2219a20eb0584b9020a08e15838e3b
harshitkkush <harshitkkush@gmail.com>
2026-01-21 17:53:30 +0530

    Initial commit - Railway deployment

---
+++ src/main/java/com/URLshortner/urlshortner/model/ShortenUrlResponse.java
@@ -0,0 +1,21 @@
+package com.URLshortner.urlshortner.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortenUrlResponse {
+
+    private String shortCode;
+    private String shortUrl;
+    private String originalUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+}
